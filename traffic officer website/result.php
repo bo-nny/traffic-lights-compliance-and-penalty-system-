2d547d3f91beaa13f8c419f50de8f661757eb687
bo-nny <77052274+bo-nny@users.noreply.github.com>
2023-07-12 12:11:42 +0300

    Add files via upload
    traffic Officer Website

---
+++ traffic officer website/result.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Results</title>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background-color: gray;
+    }
+
+    h1 {
+      background-color: #333;
+      color: #fff;
+      padding: 10px;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  </style>
+</head>
+<body>
+  <h1>Detection  Results</h1>
+  <img src="/result/exp/image.png" alt="Image 1" width="800" height="600">
+</body>
+</html>
